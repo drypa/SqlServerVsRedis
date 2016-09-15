0983c7a06b7e984c2ce333ce5035516886fee33f
DRypa <dr.drypa.k@mail.ru>
2016-09-15 17:51:24 +0300

    init

---
+++ SqlServerVsRedis/IDataSaver.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace SqlServerVsRedis
+{
+    public interface IDataSaver<in T>
+        where T : class, new()
+    {
+        void Save(T data, Guid id);
+        void Delete(Guid id);
+    }
+}
---
+++ SqlServerVsRedis/SqlServerDataSaver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SqlServerVsRedis
+{
+    internal class SqlServerDataSaver<T> : IDataSaver<T>
+        where T : class, new()
+    {
+        public void Save(T data, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
